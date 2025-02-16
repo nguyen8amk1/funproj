66ba2b5ed2fbb2d067ff862614993ba4c07c1b83
Nguyễn Trần Trung Nguyên <trungnguyen123akohshit@gmail.com>
2025-02-16 09:30:59 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 1366
+#define SCREEN_HEIGHT 768
 #define MAX_RECORDS 10000
 
 typedef struct {
@@ -16,7 +16,18 @@ typedef enum {
     VIEW_SECONDARY
 } View;
 
-PositionRecord records[MAX_RECORDS];
+typedef struct {
+    Vector2 pos; 
+    float rad;
+    PositionRecord records[MAX_RECORDS]; 
+} PositionRecordedObject; 
+
+PositionRecordedObject circle = {
+    .pos = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2}, 
+    .rad = 50.0f,
+    .records = {0}
+};
+// TODO: (trantr) add more circles 
 int recordIndex = 0;
 View currentView = VIEW_MAIN;
 const Rectangle switchViewButton = {SCREEN_WIDTH - 100, 10, 80, 30};
@@ -35,8 +46,6 @@ typedef struct {
     bool isScrubbing;
 
     // draw 
-    Vector2 circlePos;
-    float circleRadius;
     Rectangle playButton;
     Rectangle pauseButton;
     Rectangle rewindButton;
@@ -52,9 +61,9 @@ UIState ui_state = {};
 
 void update_first_view_state(FirstViewState *first_view_state) {
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-        float dist = (ui_state.mousePos.x - first_view_state->circlePos.x) * (ui_state.mousePos.x - first_view_state->circlePos.x) + 
-            (ui_state.mousePos.y - first_view_state->circlePos.y) * (ui_state.mousePos.y - first_view_state->circlePos.y);
-        if (dist <= first_view_state->circleRadius * first_view_state->circleRadius) {
+        float dist = (ui_state.mousePos.x - circle.pos.x) * (ui_state.mousePos.x - circle.pos.x) + 
+            (ui_state.mousePos.y - circle.pos.y) * (ui_state.mousePos.y - circle.pos.y);
+        if (dist <= circle.rad * circle.rad) {
             first_view_state->isDragging = true;
             recordIndex = 0;
             first_view_state->startTime = ui_state.currentTime;
@@ -70,9 +79,9 @@ void update_first_view_state(FirstViewState *first_view_state) {
     }
 
     if (first_view_state->isDragging && recordIndex < MAX_RECORDS) {
-        records[recordIndex].x = ui_state.mousePos.x;
-        records[recordIndex].y = ui_state.mousePos.y;
-        records[recordIndex].t = ui_state.currentTime - first_view_state->startTime;
+        circle.records[recordIndex].x = ui_state.mousePos.x;
+        circle.records[recordIndex].y = ui_state.mousePos.y;
+        circle.records[recordIndex].t = ui_state.currentTime - first_view_state->startTime;
         recordIndex++;
     }
 
@@ -91,13 +100,13 @@ void update_first_view_state(FirstViewState *first_view_state) {
         first_view_state->playbackIndex = ((ui_state.mousePos.x - first_view_state->timeline.x) / first_view_state->timeline.width) * recordIndex;
         if (first_view_state->playbackIndex < 0) first_view_state->playbackIndex = 0;
         if (first_view_state->playbackIndex >= recordIndex) first_view_state->playbackIndex = recordIndex - 1;
-        first_view_state->circlePos.x = records[first_view_state->playbackIndex].x;
-        first_view_state->circlePos.y = records[first_view_state->playbackIndex].y;
+        circle.pos.x = circle.records[first_view_state->playbackIndex].x;
+        circle.pos.y = circle.records[first_view_state->playbackIndex].y;
     }
 
     if (first_view_state->isPlaying && !first_view_state->isPaused && first_view_state->playbackIndex < recordIndex) {
-        first_view_state->circlePos.x = records[first_view_state->playbackIndex].x;
-        first_view_state->circlePos.y = records[first_view_state->playbackIndex].y;
+        circle.pos.x = circle.records[first_view_state->playbackIndex].x;
+        circle.pos.y = circle.records[first_view_state->playbackIndex].y;
         first_view_state->playbackIndex++;
         if (first_view_state->playbackIndex >= recordIndex) {
             first_view_state->isPlaying = false;
@@ -105,17 +114,17 @@ void update_first_view_state(FirstViewState *first_view_state) {
     }
 
     if (first_view_state->isDragging) {
-        first_view_state->circlePos = ui_state.mousePos;
+        circle.pos = ui_state.mousePos;
     }
 }
 
 void draw_first_view(const FirstViewState *first_view_state) {
     for (int i = 1; i < recordIndex; i++) {
-        float intensity = records[i].t / records[recordIndex - 1].t;
+        float intensity = circle.records[i].t / circle.records[recordIndex - 1].t;
         Color pathColor = (Color){0, 0, 255, (unsigned char)(intensity * 255)};
-        DrawLineV((Vector2){records[i - 1].x, records[i - 1].y}, (Vector2){records[i].x, records[i].y}, pathColor);
+        DrawLineV((Vector2){circle.records[i - 1].x, circle.records[i - 1].y}, (Vector2){circle.records[i].x, circle.records[i].y}, pathColor);
     }
-    DrawCircleV(first_view_state->circlePos, first_view_state->circleRadius, first_view_state->isDragging ? DARKGRAY : BLUE);
+    DrawCircleV(circle.pos, circle.rad, first_view_state->isDragging ? DARKGRAY : BLUE);
     DrawRectangleRec(first_view_state->playButton, GREEN);
     DrawRectangleRec(first_view_state->pauseButton, ORANGE);
     DrawRectangleRec(first_view_state->rewindButton, RED);
@@ -143,14 +152,12 @@ void update_second_view_state(SecondViewState *second_view_state) {
     }
 
     // Handle panning
-    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+    if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
         const Vector2 delta = GetMouseDelta();
         second_view_state->offset.x += delta.x;
         second_view_state->offset.y += delta.y;
     }
 }
-
-
 
 void draw_second_view(const SecondViewState *second_view_state) {
     // Draw grid
@@ -169,54 +176,48 @@ void draw_second_view(const SecondViewState *second_view_state) {
     DrawLine(0, second_view_state->offset.y, SCREEN_WIDTH, second_view_state->offset.y, BLACK);
     DrawLine(second_view_state->offset.x, 0, second_view_state->offset.x, SCREEN_HEIGHT, BLACK);
 
-    // Plot y = sin(x)
-
-    // for (float sx = 0; sx < SCREEN_WIDTH; sx += precision) {
-    //     // Convert screen x to graph x
-    //     float x1 = (sx - second_view_state->offset.x) / second_view_state->zoom;
-    //     float x2 = ((sx + precision) - second_view_state->offset.x) / second_view_state->zoom;
-    //
-    //     // Compute function values
-    //     float y1 = (x1);
-    //     float y2 = (x2);
-    //
-    //     // Convert graph y to screen y
-    //     float sy1 = second_view_state->offset.y - y1 * second_view_state->zoom;
-    //     float sy2 = second_view_state->offset.y - y2 * second_view_state->zoom;
-    //
-    //     // Draw function segment
-    //     DrawLine(sx, sy1, sx + precision, sy2, functionColor);
-    // }
-
     const Color functionColor = RED;
     DrawText("Graphing Mode", 10, 10, 20, DARKGRAY);
+
+    // FIXME: (nttn) fix this, i want to render the graph 
     for (int i = 1; i < recordIndex; i++) {
-        const float t1 = records[i - 1].t;
-        const float t2 = records[i].t;
-        const float x1 = (t1 - second_view_state->offset.x)/second_view_state->zoom;
-        const float x2 = (t2 - second_view_state->offset.x)/second_view_state->zoom;
-
-        const float y1 = x1; 
-        const float y2 = x2; 
-
-        const float sy1 = second_view_state->offset.y - y1 * second_view_state->zoom;
-        const float sy2 = second_view_state->offset.y - y2 * second_view_state->zoom;
-        DrawLine(x1, sy1, x2, sy2, functionColor);
+        // const float t1 = circle.records[i - 1].t;
+        // const float t2 = circle.records[i].t;
+        // const float x1 = (t1 - second_view_state->offset.x)/second_view_state->zoom;
+        // const float x2 = (t2 - second_view_state->offset.x)/second_view_state->zoom;
+        //
+        // const float y1 = x1; 
+        // const float y2 = x2; 
+        //
+        // const float sy1 = second_view_state->offset.y - y1 * second_view_state->zoom;
+        // const float sy2 = second_view_state->offset.y - y2 * second_view_state->zoom;
+        // printf("render data: x1: %f, y1: %f\n",  x1, sy1);
+        // printf("record data: x1: %f, y1: %f, t1: %f\n",  circle.records[i-1].x, circle.records[i-1].y, circle.records[i-1].t);
+        //DrawLine(x1, sy1, x2, sy2, functionColor);
+        
+        // Screen space rendering
+        const float t1 = circle.records[i - 1].t;
+        const float t2 = circle.records[i].t;
+        const float y1 = circle.records[i - 1].y;
+        const float y2 = circle.records[i].y;
+        DrawLine(t1*100, y1, t2*100, y2, functionColor);
     }
+}
+
+int* split_cycles_from_position_record_data(const PositionRecord *data) {
+    // TODO: (nttn) 
 }
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Drag the Circle with Recording and Playback Controls");
     SetTargetFPS(60);
     FirstViewState first_view_state = {
-        .circlePos = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2}, 
-        .circleRadius = 50.0f, 
-
         .playButton = {10, SCREEN_HEIGHT - 50, 80, 30}, 
         .pauseButton = {100, SCREEN_HEIGHT - 50, 80, 30}, 
         .rewindButton = {190, SCREEN_HEIGHT - 50, 80, 30}, 
         .timeline = {10, SCREEN_HEIGHT - 20, SCREEN_WIDTH - 20, 5}
-    }; 
+    };
+
     SecondViewState second_view_state = {
         .offset = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, // Center of screen
         .zoom = 50.0f // Pixels per unit
@@ -255,3 +256,4 @@ int main() {
     CloseWindow();
     return 0;
 }
+
